bb4869f2c759fdbbf127ad2a98046e7b97723b75
O-keita <o.keita@alustudent.com>
2024-08-11 22:10:28 +0000

    team portfolio done

--- app.js
+++ app.js
@@ -25,3 +25,43 @@ navLinks.forEach((link) => {
     nav.style.left = "-1000px";
   });
 });
+
+let nextBtn = document.querySelector("#next");
+let prevBtn = document.querySelector("#prev");
+let slides = document.querySelectorAll(".slide");
+let changeSlide = 0;
+// console.log(changeSlide);
+nextBtn.addEventListener("click", function () {
+  slides.forEach(function (slide, index) {
+    if (slide.classList.contains("show") === true) {
+      changeSlide = index + 1;
+      slide.classList.remove("show");
+    }
+  });
+  //   console.log(changeSlide);
+  if (changeSlide < slides.length) {
+    slides[changeSlide].classList.add("show");
+  } else {
+    changeSlide = 0;
+    slides[changeSlide].classList.add("show");
+  }
+});
+// console.log(changeSlide);
+prevBtn.addEventListener("click", function () {
+  slides.forEach(function (slide, index) {
+    if (slide.classList.contains("show") === true) {
+      changeSlide = index - 1;
+      slide.classList.remove("show");
+    }
+  });
+  // console.log(changeSlide);
+
+  if (changeSlide < slides.length && changeSlide > -1) {
+    slides[changeSlide].classList.add("show");
+  } else {
+    // console.log(slides.length);
+
+    changeSlide = slides.length - 1;
+    slides[changeSlide].classList.add("show");
+  }
+});
